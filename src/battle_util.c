8c651a2a620998268a6a5da0392390215f94cbfb
Psychoreality <jacob.j.head@gmail.com>
2025-12-04 18:32:52 +0800

    okay generalist might work now

--- src/battle_util.c
+++ src/battle_util.c
@@ -8117,7 +8117,7 @@ static inline uq4_12_t GetSameTypeAttackBonusModifier(struct DamageContext *ctx)
     else if (gBattleStruct->pledgeMove && IS_BATTLER_OF_TYPE(BATTLE_PARTNER(ctx->battlerAtk), ctx->moveType))
         return (ctx->abilityAtk == ABILITY_ADAPTABILITY) ? UQ_4_12(2.0) : UQ_4_12(1.5);
     else if (!IS_BATTLER_OF_TYPE(ctx->battlerAtk, ctx->moveType) || ctx->move == MOVE_STRUGGLE || ctx->move == MOVE_NONE)
-        return UQ_4_12(1.0);
+        return (ctx->abilityAtk == ABILITY_GENERALIST) ? UQ_4_12(1.2) : UQ_4_12(1.0);
     return (ctx->abilityAtk == ABILITY_ADAPTABILITY) ? UQ_4_12(2.0) : UQ_4_12(1.5);
 }
 
